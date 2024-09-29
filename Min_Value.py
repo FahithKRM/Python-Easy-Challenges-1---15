ddade830bb865250083e66f62925107ee34afeef
Fahith KRM <141358141+FahithKRM@users.noreply.github.com>
2024-09-29 21:52:53 +0530

    Create Min_Value.py

---
+++ Min_Value.py
@@ -0,0 +1,9 @@
+def MinValue():
+  Number_List = [12, 53, 19, 98, 45]
+  min_value = Number_List[0]
+  for _ in range(len(Number_List)):
+    if min_vale < Number_List[i]:
+      min_value = Number_List[i]
+  return min_value 
+
+print(f"Minimum value is these Number_List 12, 53, 19, 98, 45 ", MinValue())
