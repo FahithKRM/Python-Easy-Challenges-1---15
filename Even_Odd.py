ff8aaae199dbcb178e2dc21fa48473fcb8c435db
Fahith KRM <141358141+FahithKRM@users.noreply.github.com>
2024-09-29 21:42:32 +0530

    Update Even_Odd.py

--- Even_Odd.py
+++ Even_Odd.py
@@ -1,14 +1,14 @@
 # code - 01
 """number = int(input("Enter the number : "))
 
-if number % 2 :
+if number % 2 == 0:
   print(f"{number} is Even")
 else :
   print(f"{number} is Odd")"""
 
 # code - 02 
 def Even_Odd(number):
-   if number % 2 :
+   if number % 2 == 0:
      print(f"{number} is Even")
    else :
      print(f"{number} is Odd")
