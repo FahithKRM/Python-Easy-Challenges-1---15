4e60cd14aa88084e031c9f6637174c517ef5d35c
Fahith KRM <141358141+FahithKRM@users.noreply.github.com>
2024-09-29 22:07:45 +0530

    Update Min_Value.py

--- Min_Value.py
+++ Min_Value.py
@@ -1,7 +1,7 @@
 def MinValue():
   Number_List = [12, 53, 19, 98, 45]
   min_value = Number_List[0]
-  for _ in range(len(Number_List)):
+  for i in range(len(Number_List)):
     if min_vale < Number_List[i]:
       min_value = Number_List[i]
   return min_value 
