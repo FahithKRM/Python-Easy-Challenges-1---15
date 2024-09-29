025119422f8a3297b8a08337baa9d71bd79f8901
Fahith KRM <141358141+FahithKRM@users.noreply.github.com>
2024-09-29 21:33:33 +0530

    Create Even_Odd.py

---
+++ Even_Odd.py
@@ -0,0 +1,17 @@
+# code - 01
+"""number = int(input("Enter the number : "))
+
+if number % 2 :
+  print(f"{number} is Even")
+else :
+  print(f"{number} is Odd")"""
+
+# code - 02 
+def Even_Odd(number):
+   if number % 2 :
+     print(f"{number} is Even")
+   else :
+     print(f"{number} is Odd")
+
+number = int(input("Enter the number : "))
+Even_Odd(number)
